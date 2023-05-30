b0bbdb2f8248f82c261fcbdc917e9c1f00fec635
Juan Andres Orozco <120572675+AorozcoEys@users.noreply.github.com>
2023-05-30 16:43:20 -0500

    Now is working with MongoDB Atlas

--- backend/src/utils/database.js
+++ backend/src/utils/database.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(`mongodb+srv://BYMADEINCOL:9Dy2Ebrp@cluster0.js9g8uo.mongodb.net/?retryWrites=true&w=majority`, {
+    await mongoose.connect("mongodb+srv://admin1:9Dy2Ebrp@cluster0.js9g8uo.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      auth: {
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD
-      }
     });
     console.log('MongoDB connected');
   } catch (error) {
